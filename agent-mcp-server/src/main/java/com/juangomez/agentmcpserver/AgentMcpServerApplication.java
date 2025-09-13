520339476f4da17ad6c9c97c01538a01bf12ac98
juan <juanjuangmo@gmail.com>
2025-09-13 13:20:26 +0200

    Initial commit

---
+++ agent-mcp-server/src/main/java/com/juangomez/agentmcpserver/AgentMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.juangomez.agentmcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentMcpServerApplication.class, args);
+    }
+
+}
